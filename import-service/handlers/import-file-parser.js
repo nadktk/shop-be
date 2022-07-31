6a46ac0b00caa13dd740bd726ac8fa9f624a3db7
NKATKOVA <nadezhda.katkova@gmail.com>
2022-07-31 22:17:57 +0200

    added function to parse csv file

--- import-service/handlers/import-file-parser.js
+++ import-service/handlers/import-file-parser.js
@@ -1,4 +1,13 @@
+import { S3 } from 'aws-sdk';
+import csvParser from 'csv-parser';
+
 import { successResponse, serverErrorResponse } from '../../shared/utils';
+
+const { REGION, BUCKET } = process.env;
+
+const s3 = new S3({
+    region: REGION,
+});
 
 /**
  * importFileParser
@@ -14,7 +23,32 @@ export const importFileParser = async (event) => {
             )}
         `);
 
-        return successResponse('Hello world importFileParser');
+        const key = event.Records[0].s3.object.key;
+
+        const params = {
+            Bucket: BUCKET,
+            Key: key,
+        }
+
+        const s3Srteam = s3.getObject(params).createReadStream();
+        const result = [];
+
+        const fileStream = new Promise((res, rej) => {
+            s3Srteam.pipe(csvParser())
+                .on('data', (data) => {
+                    console.log(data);
+                    result.push(data);
+                })
+                .on('error', (err) => {
+                    console.log(err);
+                    rej(err);
+                })
+                .on('end', () => res());
+        });
+
+        await fileStream;
+
+        return successResponse(result);
     } catch (e) {
         console.log(e);
 
